622e7e8ae71d13f600f30dea296ed41d6656b43f
lipakshirana36 <lipakshirana36@gmail.com>
2026-03-23 19:40:54 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//})
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/feedback-app/",
+  plugins: [react()],
+  base: '/feedback-app/',
 })
